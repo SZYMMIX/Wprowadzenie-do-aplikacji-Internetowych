5f6da5db50b567150b9bcfba685288bcfdd08f22
Szymon Cimochowski <szycim10@gmail.com>
2026-01-08 07:24:03 +0100

    Implement JWT authentication filter and update security configuration

---
+++ lab4/orders/src/main/java/com/example/orders/JwtAuthFilter.java
@@ -0,0 +1,50 @@
+package com.example.orders;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.security.Key;
+import java.util.Collections;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private static final String SECRET_STRING = "mojbardzotajnykluczktorymusimiecconajmniej32znaki";
+    private static final Key SECRET_KEY = Keys.hmacShaKeyFor(SECRET_STRING.getBytes());
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            try {
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(SECRET_KEY)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String email = claims.getSubject();
+
+                if (email != null) {
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            } catch (Exception e) {
+
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
